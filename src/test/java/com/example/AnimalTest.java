4d585c99e4bc79ab0a80b372880ccfdd841f7cda
Yaroslav Dranishnikov <slik-klick@mail.ru>
2024-04-24 14:46:17 +0300

    Sprint_6

---
+++ src/test/java/com/example/AnimalTest.java
@@ -0,0 +1,40 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class AnimalTest {
+    @Test()
+    public void getFoodReturnException() throws Exception {
+        Animal animal = new Animal();
+        String badFoodRequest = "asd";
+        try {
+            animal.getFood(badFoodRequest);
+            Assert.fail("Exception thrown");
+        } catch (Exception thrown) {
+            assertNotEquals(badFoodRequest, thrown.getMessage());
+        }
+    }
+
+    @Test
+    public void getFamilyCorrectReturnValue() {
+        Animal animal = new Animal();
+        String actualFamily = animal.getFamily();
+        String expectedFamily = "Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи";
+        assertEquals(expectedFamily, actualFamily);
+    }
+
+    @Test
+    public void getFoodValueOfHerbivoreReturn() throws Exception {
+        Animal animal = new Animal();
+        List<String> expectedEat = List.of("Трава", "Различные растения");
+        List<String> actualEat = animal.getFood("Травоядное");
+        assertEquals(expectedEat, actualEat);
+    }
+
+}
